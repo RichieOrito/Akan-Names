f5a2f639be832741c0b6dd0d8df200fe568b69f9
RichieOrito <richard.omondi@student.moringaschool.com>
2021-11-14 16:17:22 +0300

    This commit adds arrays for days and names

---
+++ main.js
@@ -0,0 +1,5 @@
+//Arrays of Akan Male , Female Names , Days of The Week
+let maleNames = ["Kwasi", "Kwadow", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame",];
+let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+let daysOfWeek =[ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
