b72c6248d912ef68ec2cb4d1a00a593c5fcded6d
RichieOrito <richard.omondi@student.moringaschool.com>
2021-11-15 07:22:33 +0300

    This commit adds function getName

--- main.js
+++ main.js
@@ -1,42 +1,106 @@
-//Arrays of Akan Male , Female Names , Days of The Week
-let maleNames = ["Kwasi", "Kwadow", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame",];
-let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-let daysOfWeek =[ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let daysOfTheWeek = 
+[
+"Sunday",
+"Monday",
+"Tuesday",
+"Wednesday",
+"Thursday",
+"Friday",
+"Saturday"
+];
 
-function validateForm(){
-  let gender= document.getElementsByName("gender");
-  let dateInput = document.getElementByNameById("date").value;
-  let date = new Date(dateInput);
+let maleNames = 
+[
+"Kwasi",
+"Kwadwo", 
+"Kwabena", 
+"Kwaku", 
+"Yaw",
+"Kofi",
+"Kwame"];
 
-  if (!!date.valueOf()){
-    let year = date.getFullYear().toString();
-    let MM = date.getMonth()+1;
-    let DD = date.getDate();
-    let CC = parseInt(year.slice(0,2));
-    let YY = parseInt(year.slice(2,4));
-    let dayofWeek = (((CC / 4)-2* CC - 1) + (5 * YY / 4)+ (26 * (MM+1) / 10) +DD) % 7;
-    let dayOfWeek = date.getDay();
-    return dayOfWeek;
-  }
-  else{
-    alert("Select your date of birth");
-    return;
-  }
-  }
+let femaleNames = 
+[
+"Akosua", 
+"Adwoa", 
+"Abenaa",
+"Akua",
+"Yaa", 
+"Afua", 
+"Ama"
+];
 
-  function getGender() {
+function getBirthdate() {
+
+    let dateInput = document.getElementById("date").value;
+
+    let date = new Date(dateInput);
+
+    if (!!date.valueOf()) {
+
+        let year = date.getFullYear().toString();
+        //  let dayOfWeek = (((century / 4) - 2 * century - 1) + (5 *year/ 4) + (26 * (month + 1) / 10) + dayOfMonth) % 7;
+        let dayOfWeek = date.getDay();
+
+        return dayOfWeek;
+
+    } 
+    
+    else {
+        alert("Please select your birthday.");
+        return;
+    }
+}
+
+// function to get gender
+function getGender() {
+
     let gender;
+
     if ((form.gender[0].checked == true) || (form.gender[1].checked == true)) {
+
         if (form.gender[0].checked == true) {
             gender = 0;
-        } 
-        else if (form.gender[1].checked == true) {
+
+        } else if (form.gender[1].checked == true) {
             gender = 1;
         }
         return gender;
+
     } 
+    
     else {
         alert("Please select your gender!");
+        return;
+    }
+}
+
+function getName() {
+
+    let selectedGender = getGender();
+
+    let dayOfWeek = getBirthdate();
+
+    if ((!!dayOfWeek.valueOf() === true) || (dayOfWeek == 0))  {
+
+        if (selectedGender === 0) {
+
+            let result ="Hey" + ' ' +"Your Akan name is " + maleNames[dayOfWeek] + ".";
+
+            document.getElementById("result").innerHTML = alert(result);
+
+        } 
+        
+        else if (selectedGender === 1) {
+
+            let result ="Hey" + ' ' +"Your Akan name is " + femaleNames[dayOfWeek] + ".";
+
+            document.getElementById("result").innerHTML = alert(result);
+        }
+
+    }
+    
+    else{
         return;
     }
 }
